08e9ac826f9a8bf7c94d05cab541c71d4b9046df
Nrin <Nrin3141@users.noreply.github.com>
2019-10-05 16:49:08 +0200

    Remove express scaffold

--- sketch.js
+++ sketch.js
@@ -1,13 +1,6 @@
 p5.disableFriendlyErrors = true;
 let w = window.innerWidth;
 let h = window.innerHeight;
-let dot;
-let interval;
-let font;
-let points;
-let points2;
-let dots = [];
-let timerId = null;
 function preload() {
 }
 function setup() {
