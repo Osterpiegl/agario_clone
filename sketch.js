9e7cb82aa905af26f8d4b4d12a88b6997a278ffc
Nrin <Nrin3141@users.noreply.github.com>
2019-10-05 16:42:59 +0200

    Add simple structure

---
+++ sketch.js
@@ -0,0 +1,17 @@
+p5.disableFriendlyErrors = true;
+let w = window.innerWidth;
+let h = window.innerHeight;
+let dot;
+let interval;
+let font;
+let points;
+let points2;
+let dots = [];
+let timerId = null;
+function preload() {
+}
+function setup() {
+}
+function draw() {
+}
+
