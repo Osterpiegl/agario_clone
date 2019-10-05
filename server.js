b4c4bc4269f956c9626290348f8913f866bfa455
Nrin <Nrin3141@users.noreply.github.com>
2019-10-05 17:20:22 +0200

    Add rest of tutorial, add nodemon

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+let app = require('express')();
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -8,7 +8,22 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('a user disconnected');
+    });
 });
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+    });
+  });
+
+  io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
+    });
+  });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
