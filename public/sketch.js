dbad7a1413f7d65493df76521d141c8958ed2ba9
Timo Angerer <code@timoangerer>
2019-10-05 17:48:40 +0200

    add name and color to player

--- public/sketch.js
+++ public/sketch.js
@@ -4,10 +4,12 @@ function preload() {}
 let m = 0;
 
 class Player {
-  constructor(x = 1, y = 1, size = 10) {
+  constructor(x = 1, y = 1, size = 10, name = "", color = "#123") {
     this.x = x;
     this.y = y;
     this.size = size;
+    this.name = name;
+    this.color = color;
   }
 
   updatePos(x, y) {
@@ -16,6 +18,8 @@ class Player {
   }
 
   draw() {
+    let c = color(this.color);
+    fill(c);
     ellipse(this.x, this.y, this.size, this.size);
   }
 }
@@ -35,22 +39,21 @@ function draw() {
 }
 
 function keyPressed() {
-    let keyIndex = -1;
-    console.log(key)
-    if (key >= 'a' && key <= 'z') {
-      keyIndex = key.charCodeAt(0) - 'a'.charCodeAt(0);
-    }
-    if (keyIndex === -1) {
-      // If it's not a letter key, clear the screen
-      background(230);
-    } else {
-      // It's a letter key, fill a rectangle
-      randFill_r = Math.floor(Math.random() * 255 + 1);
-      randFill_g = Math.floor(Math.random() * 255 + 1);
-      randFill_b = Math.floor(Math.random() * 255 + 1);
-      fill(randFill_r, randFill_g, randFill_b);
-      let x = map(keyIndex, 0, 25, 0, width - rectWidth);
-      rect(x, 0, rectWidth, height);
-    }
+  let keyIndex = -1;
+  console.log(key);
+  if (key >= "a" && key <= "z") {
+    keyIndex = key.charCodeAt(0) - "a".charCodeAt(0);
   }
-  
+  if (keyIndex === -1) {
+    // If it's not a letter key, clear the screen
+    background(230);
+  } else {
+    // It's a letter key, fill a rectangle
+    randFill_r = Math.floor(Math.random() * 255 + 1);
+    randFill_g = Math.floor(Math.random() * 255 + 1);
+    randFill_b = Math.floor(Math.random() * 255 + 1);
+    fill(randFill_r, randFill_g, randFill_b);
+    let x = map(keyIndex, 0, 25, 0, width - rectWidth);
+    rect(x, 0, rectWidth, height);
+  }
+}
